2fa2e7aa49a08b6623c66ad1ed488ef0417862e4
Adarsh Prakash <adarsh.prakash@aquera.com>
2024-11-14 21:13:14 +0530

    Laying down foundation part-1

--- App.js
+++ App.js
@@ -1,18 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-const heading2 = React.createElement("h2", {}, "I am h2 tag");
-const heading1 = React.createElement("h1", {}, "I am h1 tag");
-const child1 = React.createElement("div", { id: "child1" }, [
-    heading1,
-    heading2
-]);
-const child2 = React.createElement("div", { id: "child2" }, [
-    heading1,
-    heading2
-]);
-const parent = React.createElement("div", { id: "parent" }, [child1, child2]);
+// const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+
+// JSX transpile before it reaches to JS Engine => Parcel => Babel
+
+// JSX => React.createElement => ReactElement-JS Object => HTMLElement(render)
+const jsxHeading = <h1 id="heading">Namaste React from JSX.</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
 
+root.render(jsxHeading);
